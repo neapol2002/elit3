ae6bc3e0de922970df590c3e97b0d07671a73839
neapol2002 <73583307+neapol2002@users.noreply.github.com>
2022-08-26 17:13:16 +0400

    lg upd

--- js/lg.js
+++ js/lg.js
@@ -1,4 +1,12 @@
 lightGallery(document.getElementById("gallery-container"), {
+licenseKey: '7EC452A9-0CFD441C-BD984C7C-17C8456E',
 speed: 500,
 plugins: [lgFullscreen, lgShare],
+
+mobileSettings: {
+          controls: false,
+          showCloseIcon: true,
+          download: false
+}
+
 });
