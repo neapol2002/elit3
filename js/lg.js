c5e3418bc5c8eb452b40e93e8ee192df1c491165
neapol2002 <73583307+neapol2002@users.noreply.github.com>
2022-09-07 14:04:25 +0400

    lg js upd

--- js/lg.js
+++ js/lg.js
@@ -1,7 +1,8 @@
 lightGallery(document.getElementById("gallery-container"), {
 licenseKey: '7EC452A9-0CFD441C-BD984C7C-17C8456E',
 speed: 500,
-plugins: [lgFullscreen, lgShare],
+download: false,
+plugins: [lgFullscreen, lgZoom],
 
 mobileSettings: {
           controls: false,
